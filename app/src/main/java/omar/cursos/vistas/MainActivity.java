77ca6d50f7c73c54275a1577ef38e32a9dbd6415
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2019-05-26 07:03:26 -0500

    Commit

--- app/src/main/java/omar/cursos/vistas/MainActivity.java
+++ app/src/main/java/omar/cursos/vistas/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Prueba
+        //Prueba 2
         setContentView(R.layout.activity_main);
 
 
